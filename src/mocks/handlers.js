896150daec827d65745c46153ad54b4934e1fd9d
Malin  Sjögren <mail2malins@gmail.com>
2024-04-04 14:13:06 +0000

    Add Navbar test

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,14 +6,14 @@ export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
     return res(
       ctx.json({
-        pk: 2,
-        username: "brian",
+        pk: 3,
+        username: "Malin",
         email: "",
         first_name: "",
         last_name: "",
-        profile_id: 2,
+        profile_id: 3,
         profile_image:
-          "https://res.cloudinary.com/dgjrrvdbl/image/upload/v1/media/../default_profile_qdjgyp",
+        "https://res.cloudinary.com/dqhoo921b/image/upload/v1/media/../default_profile_qdjgyp",
       })
     );
   }),
